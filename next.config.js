8b72e00832a081c828eeb15aa9d99782e4808d5c
bupd <bupdprasanth@gmail.com>
2023-11-26 06:54:49 +0530

    pwa test
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disableDevLogs: true,
+  disable: process.env.NODE_ENV === "development",
+});
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
